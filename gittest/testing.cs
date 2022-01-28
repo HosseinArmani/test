5a650ffcf2eadb98d26558504108e4d074e61ca3
hossein <hossein002hs@gmail.com>
2022-01-28 22:22:44 +0330

    Add project files.

---
+++ gittest/testing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittest
+{
+  public static class testing
+    {
+        public static string GitTest()
+        {
+            return null;
+        }
+    }
+}
